facee2d5b6bb416b8bcc5559b0868ce52cec2f49
c-koster <52757828+c-koster@users.noreply.github.com>
2021-08-27 17:02:22 -0400

    constants changes and more demos

--- clock.py
+++ clock.py
@@ -1,5 +1,6 @@
 from kelvin_to_rgb import convert_K_to_RGB
-kvals = [1000,1500,2000,2500,4000,8000,16000,32000]
+kvals = list(range(1000,6100,100))
+print(kvals)
 for k in kvals:
     print("{}K temperature has has rgb values: {}".format(k,convert_K_to_RGB(k)))
 
@@ -11,8 +12,8 @@ import board
 import neopixel
 
 pi_pin = board.D18
-numpix = 144
-brightness = 1.0
+numpix = 150
+brightness = .3
 pixels = neopixel.NeoPixel(pi_pin, numpix, brightness=brightness)
 
 # morning BLUE light hours
@@ -28,10 +29,10 @@ end_night = "22:00:00"
 color_change = False
 
 for k in kvals:
-    color = convert_k_to_rgb(k)
+    color = convert_K_to_RGB(k)
 
     pixels.fill(color)
-    time.sleep(5)
+    time.sleep(1)
 
 pixels.fill((0,0,0))
 exit(0)
