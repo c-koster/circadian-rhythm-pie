ef8afb1c9cb9c7ec48202ec952388f61c5f78e6e
c-koster <52757828+c-koster@users.noreply.github.com>
2021-08-27 17:52:10 -0400

    trying out my fav 3 colors

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-### hello
+### Desk Lamp Mini Project
+Hello!
+
+Something I struggle with at Middlebury a lot: the dorms have really awfullighting, so I've had to use string/desk lights every semester instead of using the light switch.
+
+I'm already using a pi as a Spotify Connect speaker so I'm attaching a neopixel light strip to it, which modulates its color based upon time of day (less than 2700K for night time and more than 4000K in the morning).
+
+
+Maybe I'll steal John's disco/lightshow code and have it respond to a 'disco' or 'off' text message.
 
 ### Sources:
 
 - Kelvin to RGB converter: https://gist.github.com/petrklus/b1f427accdf7438606a6
+- template for clock code: https://learn.adafruit.com/circadian-pi-desk-light/circuitpython-code
+- I used John Cambefort's hardware wiring from the TEMPO project
--- clock.py
+++ clock.py
@@ -1,68 +1,77 @@
-from kelvin_to_rgb import convert_K_to_RGB
-kvals = list(range(1000,6100,100))
-print(kvals)
-for k in kvals:
-    print("{}K temperature has has rgb values: {}".format(k,convert_K_to_RGB(k)))
+"""
+clock.py
 
-
+Determines time of day and writes pixel data to the light strip
+This should run on boot.
+"""
 
 import datetime
 import time
 import board
 import neopixel
 
+from kelvin_to_rgb import convert_K_to_RGB
+#kvals = list(range(1000,6100,100))
+#[print("{}K temperature has has rgb values: {}".format(k,convert_K_to_RGB(k))) for k in kvals]
+
+OFF_ZERO = (0,0,0)
+
 pi_pin = board.D18
 numpix = 150
-brightness = .3
-pixels = neopixel.NeoPixel(pi_pin, numpix, brightness=brightness)
+brightness = .1
 
 # morning BLUE light hours
 # BLUE light is stimulating
-start_morning = "06:00:00"
-end_morning = "10:00:00"
+start_morning = "07:30:00"
+end_morning = "09:00:00"
 
-# evening RED light hours
-# RED light is calming allows melatonin production to increase
-start_night = "18:00:00"
-end_night = "22:00:00"
+# evening hours means dim-ish red lights
+start_evening = "19:00:00"
+#end_evening = "22:00:00" # no end_evening needed as the lights switch immediately
 
-color_change = False
+# third option: night hours means barely visible (or just off). End at 6am
+start_night = "22:00:00"
+end_night = "06:00:00"
 
-for k in kvals:
-    color = convert_K_to_RGB(k)
+with pixels as neopixel.NeoPixel(pi_pin, numpix, brightness=brightness):
 
-    pixels.fill(color)
-    time.sleep(1)
-
-pixels.fill((0,0,0))
-exit(0)
-
-"""
-while True:
-    date_string = datetime.datetime.now().strftime("%H:%M:%S" )
-
-    if date_string == start_morning:
-        color = (0, 0, 255)
-        color_change = True
-
-    elif date_string == end_morning:
-        color = (0, 0, 0)
-        color_change = True
-
-    elif date_string == start_night:
-        color = (255, 0, 0)
-        color_change = True
-
-    elif date_string == end_night:
-        color = (0, 0, 0)
-        color_change = True
-
-    else:
-        time.sleep(1)
-
-    # update neopixel strip with new colors
-    if color_change:
+    for k in [1000,1500,5000]:
+        color = convert_K_to_RGB(k)
         pixels.fill(color)
-        color_change = False
-        time.sleep(1)
-"""
+        time.sleep(3)
+
+    pixels.fill(OFF_ZERO)
+    exit(0)
+
+    color_change = False
+    while True:
+        date_string = datetime.datetime.now().strftime("%H:%M:%S")
+
+        if date_string == start_morning:
+            color = convert_K_to_RGB(5000)
+            color_change = True
+
+        elif date_string == end_morning:
+            color = OFF_ZERO
+            color_change = True
+
+        elif date_string == start_evening:
+            color = convert_K_to_RGB(1500)
+            color_change = True
+
+        elif date_string == start_night:
+            color = convert_K_to_RGB(1000)
+            color_change = True
+
+        elif date_string == end_night:
+            color = OFF_ZERO
+            color_change = True
+
+        else:
+            time.sleep(1)
+
+        # update neopixel strip with new colors
+        if color_change:
+            pixels.fill(color)
+            color_change = False
+            time.sleep(1)
